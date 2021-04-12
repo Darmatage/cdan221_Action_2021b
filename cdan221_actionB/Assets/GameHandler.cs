f3aa1de980cff0755105315c7347bb933eee8958
spacepawtrol <79662048+spacepawtrol@users.noreply.github.com>
2021-04-12 18:03:29 -0400

    created gamehandler

---
+++ cdan221_actionB/Assets/GameHandler.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class GameHandler : MonoBehaviour{
+
+        public static int playerStat;
+        //public GameObject textGameObject;
+		
+		public static bool GameisPaused = false;
+        public GameObject pauseMenuUI;
+        public AudioMixer mixer;
+        public static float volumeLevel = 1.0f;
+        private Slider sliderVolumeCtrl;
+        //void Start () { UpdateScore (); }
+
+		void Awake(){
+                SetLevel (volumeLevel);
+                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+                if (sliderTemp != null){
+                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+                        sliderVolumeCtrl.value = volumeLevel;
+                }
+        }
+		
+		void Start(){
+                pauseMenuUI.SetActive(false);
+        }	
+
+        void Update(){
+                 if (Input.GetKeyDown(KeyCode.Escape)){
+                        if (GameisPaused){
+                                Resume();
+                        }
+                        else{
+                                Pause();
+                        }
+                }
+        }
+		
+		void Pause(){
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+                GameisPaused = true;
+        }
+
+        public void Resume(){
+                pauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+                GameisPaused = false;
+        }
+
+        public void RestartGame(){
+                Time.timeScale = 1f;
+                //restart the game:
+                //SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+				SceneManager.LoadScene ("MainMenu");
+        }
+		public void SetLevel (float sliderValue){
+                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+                volumeLevel = sliderValue;
+        }
+		
+        public void AddPlayerStat(int amount){
+                playerStat += amount;
+                Debug.Log("Current Player Stat = " + playerStat);
+        //      UpdateScore ();
+        }
+
+        //void UpdateScore () {
+        //        Text scoreTemp = textGameObject.GetComponent<Text>();
+        //        scoreTemp.text = "Score: " + score; }
+
+        public void StartGame(){
+                SceneManager.LoadScene("Scene1");
+        }
+
+        public void RestartGame(){
+                SceneManager.LoadScene("MainMenu");
+        }
+
+        public void QuitGame(){
+                #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                Application.Quit();
+                #endif
+        }
+}
