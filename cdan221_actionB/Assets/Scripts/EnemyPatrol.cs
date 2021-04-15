2b84da3e1cad671ceb5043c2a26bf902d67c7358
JasonWiser <JasonWiserArt@gmail.com>
2021-04-15 14:24:28 -0400

    Added patrolling enemy

---
+++ cdan221_actionB/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour {
+
+	public float speed = 2f;
+	public Rigidbody2D rb;
+	public LayerMask groundLayers;
+	public Transform groundCheck;
+	bool isFacingRight = true;
+	RaycastHit2D hit;
+
+	void Start(){
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update(){
+		hit = Physics2D.Raycast(groundCheck.position, -transform.up, 1f, groundLayers);
+	}
+
+	void FixedUpdate(){
+		if (hit.collider != false){
+			if (isFacingRight){
+				rb.velocity = new Vector2(speed, rb.velocity.y);
+			} else { 
+				rb.velocity = new Vector2(-speed, rb.velocity.y);
+			}
+		} else {
+			isFacingRight = !isFacingRight;
+			transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+		}
+	}
+}
