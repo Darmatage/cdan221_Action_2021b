95403023436e71095324682b2ca0f2799b28fd0d
DanielGrieco <71238087+DanielGrieco@users.noreply.github.com>
2021-04-20 23:20:29 -0400

    Started my level for Violence and add a door

---
+++ cdan221_actionB/Assets/Scripts/DoorExitSimple.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorExitSimple : MonoBehaviour{
+
+      public string NextLevel = "MainMenu";
+
+      public void OnTriggerEnter2D(Collider2D other){
+            if (other.gameObject.tag == "Player"){
+                  SceneManager.LoadScene (NextLevel);
+            }
+      }
+
+}
