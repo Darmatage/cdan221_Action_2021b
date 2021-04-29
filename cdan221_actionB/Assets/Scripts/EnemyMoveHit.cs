53cb1b9c8251bf3595363a80b7f176d8855492cc
Catherine Gillard <79666491+CatAttcks@users.noreply.github.com>
2021-04-29 17:53:40 -0400

    Title Screen Art and Rough Enemy Move

--- cdan221_actionB/Assets/Scripts/EnemyMoveHit.cs
+++ cdan221_actionB/Assets/Scripts/EnemyMoveHit.cs
@@ -9,6 +9,8 @@ public class EnemyMoveHit : MonoBehaviour
     public float speed = 4f;
     private Transform target;
     public int damage = 10;
+    private bool FaceRight = true; // determine which way player is facing.
+    public bool isAlive = true;
 
     public int EnemyLives = 3;
     private Renderer rend;
@@ -39,9 +41,23 @@ public class EnemyMoveHit : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
+        
+        //NOTE: Horizontal axis: [a] / left arrow is -1, [d] / right arrow is 1
+        Vector3 hMove = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        if (isAlive == true)
+        {
+
+            // NOTE: if input is moving the Player right and Player faces left, turn, and vice-versa
+            if ((hMove.x > 0 && !FaceRight) || (hMove.x < 0 && FaceRight))
+            {
+                Enemyturn();
+            }
+        }
     }
 
-    public void OnCollisionEnter2D(Collision2D collision)
+
+
+   public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
@@ -61,6 +77,17 @@ public class EnemyMoveHit : MonoBehaviour
         }
     }
 
+    private void Enemyturn()
+    {
+        // NOTE: Switch player facing label
+        FaceRight = !FaceRight;
+
+        // NOTE: Multiply player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
     IEnumerator HitEnemy()
     {
         yield return new WaitForSeconds(0.5f);
@@ -68,8 +95,11 @@ public class EnemyMoveHit : MonoBehaviour
     }
 
     //DISPLAY the range of enemy's attack when selected in the Editor
+
     void OnDrawGizmosSelected()
+
     {
         Gizmos.DrawWireSphere(transform.position, attackRange);
     }
-}
+
+} 
