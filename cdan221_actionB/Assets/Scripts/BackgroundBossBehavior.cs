b849ccf831900a1df018aa89972b58c07429aabd
JasonWiser <JasonWiserArt@gmail.com>
2021-05-13 10:46:33 -0400

    Boss Rawr Animation and walking functionality working

--- cdan221_actionB/Assets/Scripts/BackgroundBossBehavior.cs
+++ cdan221_actionB/Assets/Scripts/BackgroundBossBehavior.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class BackgroundBossBehavior : MonoBehaviour
 {
-	
+	private Animator anim;
 	private Vector3 startScale;
-	public float scaleTime = 10f;
+	public float scaleTimeMult = 2f;
 	public float pauseTime = 5f;
 	public float pauseTimer = 0f;
 	public bool isScalingUp = false;
@@ -19,6 +19,8 @@ public class BackgroundBossBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start(){
         startScale = transform.localScale;
+		transform.localScale = SmallScale;
+		anim = gameObject.GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -26,54 +28,34 @@ public class BackgroundBossBehavior : MonoBehaviour
     {
 		if ((isScalingUp == false) && (isScalingDown == false)){
 			pauseTimer += 0.01f;
+			anim.SetBool("Walk", false);
 		}
 		
 		if (pauseTimer >= pauseTime){
 			pauseTimer = 0;
 			if ((goingBig == true) && (isScalingUp == false)){
 				isScalingUp = true;
+				anim.SetBool("Walk", true);
 				//transform.localScale = Vector3.Lerp (transform.localScale, transform.localScale * 100f, Time.deltaTime * 200f);
 				StartCoroutine(ScaleUpTheBeast());
 			}
 			else if ((goingBig == false) && (isScalingDown == false)){
 				isScalingDown = true;
+				anim.SetBool("Walk", true);
 				//transform.localScale = Vector3.Lerp (transform.localScale, transform.localScale / 100f, Time.deltaTime * 200f);
 				StartCoroutine(ScaleDownTheBeast());
 			}
 		}
     }
 	
-	// IEnumerator ScaleUpTheBeast(){
-		// for (int i = 0; i < scaleTime; i++){
-            // transform.localScale *= 1.05f;
-			// yield return null;
-        // } 
-		// yield return new WaitForSeconds(scaleTime);
-		// goingBig = false;
-		// isScalingUp = false;
-		
-	// }
-	
-	// IEnumerator ScaleDownTheBeast(){		
-		// for (int i = 0; i < scaleTime; i++){
-            // if (transform.localScale.x >= 0.1f){
-				// transform.localScale *= 0.05f;
-			// }
-			// yield return null;
-        // } 
-		// yield return new WaitForSeconds(scaleTime);
-		// goingBig = true;
-		// isScalingDown = false;
-	// }
-	
-
 
      IEnumerator ScaleUpTheBeast(){
         float progress = 0;
          
         while(progress <= 1){
              transform.localScale = Vector3.Lerp(SmallScale, BigScale, progress);
-             progress += Time.deltaTime * scaleTime;
+             progress += Time.deltaTime * scaleTimeMult;
+			 //anim.SetBool("Walk", true);
              yield return null;
         }
         transform.localScale = BigScale;
@@ -86,7 +68,8 @@ public class BackgroundBossBehavior : MonoBehaviour
          
         while(progress <= 1){
              transform.localScale = Vector3.Lerp(BigScale, SmallScale, progress);
-             progress += Time.deltaTime * scaleTime;
+             progress += Time.deltaTime * scaleTimeMult;
+			 //anim.SetBool("Walk", true);
              yield return null;
         }
         transform.localScale = SmallScale;
